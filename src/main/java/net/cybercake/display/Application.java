6f98d26e1532b6f62331e779a1e6f0b8f770f99e
CyberedCake <157924032+nfranks8036@users.noreply.github.com>
2025-01-21 02:38:48 -0500

    Add weather stuff

--- src/main/java/net/cybercake/display/Application.java
+++ src/main/java/net/cybercake/display/Application.java
@@ -10,8 +10,12 @@ import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import net.cybercake.display.args.ArgumentReader;
+
+import java.util.Objects;
 
 public class Application extends javafx.application.Application {
 
@@ -30,6 +34,7 @@ public class Application extends javafx.application.Application {
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
+        grid.setBackground(Background.fill(Color.rgb(0, 0, 0, 1.0)));
         grid.setPadding(new Insets(25, 25, 25, 25));
 
         Scene scene = new Scene(grid, 300, 275);
@@ -39,7 +44,22 @@ public class Application extends javafx.application.Application {
         Text text = new Text("No program data.");
         text.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         text.setFill(Color.rgb(255, 255, 255, 1.0));
-        grid.add(text, 0, 0, 2, 1);
+        grid.add(text, 0, 0, 1, 1);
+
+        WebView view = new WebView();
+        WebEngine engine = view.getEngine();
+
+        engine.documentProperty().addListener((obs, oldDoc, newDoc) -> {
+            if (newDoc != null) {
+                engine.executeScript(
+                        "document.body.style.backgroundColor = 'black';" +
+                                "document.body.style.color = 'white';"
+                );
+            }
+        });
+
+        engine.load("https://obscountdown.com/lwf?api_key=8bb09be56ab7764152e7a4df426c7de0&lat=37.2296566&lon=-80.4136767&unit=imperial&weather_round=0&theme=gray&lang=en&timezone=America%252FNew_York&hour_format=1&bg_color=%23303d50&font_color=%23f0f0f0&font=Cabin&background_transparency=0&scroll_speed=1&scroll_direction=left");
+        grid.add(view, 0, 0, 2, 2);
 
         stage.setMaximized(true);
         stage.setFullScreen(true);
