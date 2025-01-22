e49b9f505bf0f76a4c9dfe043e586cb70d870a86
CyberedCake <157924032+nfranks8036@users.noreply.github.com>
2025-01-21 19:55:49 -0500

    Its for the funny

--- src/main/java/net/cybercake/display/Application.java
+++ src/main/java/net/cybercake/display/Application.java
@@ -3,6 +3,8 @@ package net.cybercake.display;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -46,19 +48,23 @@ public class Application extends javafx.application.Application {
         text.setFill(Color.rgb(255, 255, 255, 1.0));
         grid.add(text, 0, 0, 1, 1);
 
-        WebView view = new WebView();
-        WebEngine engine = view.getEngine();
+//        WebView view = new WebView();
+//        WebEngine engine = view.getEngine();
+//
+//        engine.documentProperty().addListener((obs, oldDoc, newDoc) -> {
+//            if (newDoc != null) {
+//                engine.executeScript(
+//                        "document.body.style.backgroundColor = 'black';" +
+//                                "document.body.style.color = 'white';"
+//                );
+//            }
+//        });
+//
+//        engine.load("https://obscountdown.com/lwf?api_key=8bb09be56ab7764152e7a4df426c7de0&lat=37.2296566&lon=-80.4136767&unit=imperial&weather_round=0&theme=gray&lang=en&timezone=America%252FNew_York&hour_format=1&bg_color=%23303d50&font_color=%23f0f0f0&font=Cabin&background_transparency=0&scroll_speed=1&scroll_direction=left");
+//        grid.add(view, 0, 0, 2, 2);
 
-        engine.documentProperty().addListener((obs, oldDoc, newDoc) -> {
-            if (newDoc != null) {
-                engine.executeScript(
-                        "document.body.style.backgroundColor = 'black';" +
-                                "document.body.style.color = 'white';"
-                );
-            }
-        });
-
-        engine.load("https://obscountdown.com/lwf?api_key=8bb09be56ab7764152e7a4df426c7de0&lat=37.2296566&lon=-80.4136767&unit=imperial&weather_round=0&theme=gray&lang=en&timezone=America%252FNew_York&hour_format=1&bg_color=%23303d50&font_color=%23f0f0f0&font=Cabin&background_transparency=0&scroll_speed=1&scroll_direction=left");
+        Image image = new Image(this.getClass().getClassLoader().getResourceAsStream("two-black-people.gif"), 1920, 1080, false, false);
+        ImageView view = new ImageView(image);
         grid.add(view, 0, 0, 2, 2);
 
         stage.setMaximized(true);
