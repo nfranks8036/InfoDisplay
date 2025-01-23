12e5f431d40f7f8765893dfe5f31dc443bbc1852
CyberedCake <157924032+nfranks8036@users.noreply.github.com>
2025-01-22 19:28:22 -0500

    Update Application.java

--- src/main/java/net/cybercake/display/Application.java
+++ src/main/java/net/cybercake/display/Application.java
@@ -1,5 +1,7 @@
 package net.cybercake.display;
 
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -11,10 +13,12 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import net.cybercake.display.args.ArgumentReader;
+import net.cybercake.display.utils.Log;
 
 import java.io.File;
 import java.io.IOException;
@@ -64,14 +68,55 @@ public class Application extends javafx.application.Application {
 //        engine.load("https://obscountdown.com/lwf?api_key=8bb09be56ab7764152e7a4df426c7de0&lat=37.2296566&lon=-80.4136767&unit=imperial&weather_round=0&theme=gray&lang=en&timezone=America%252FNew_York&hour_format=1&bg_color=%23303d50&font_color=%23f0f0f0&font=Cabin&background_transparency=0&scroll_speed=1&scroll_direction=left");
 //        grid.add(view, 0, 0, 2, 2);
 
-        Image image = new Image(new File(new File(".", "images"), "kiss.gif").toURI().toURL().openStream(), 1920, 1080, false, false);
+        Image image = new Image(new File(new File(".", "images"), "AVIRAJ3.png").toURI().toURL().openStream(), 1920, 1080, false, false);
         ImageView view = new ImageView(image);
         grid.add(view, 1, 1, 1, 1);
-//
-//        Text text = new Text("Person: Noah F");
+
+        Text text = new Text("YOU ARE GEEKED");
+        text.setFont(Font.font("Tahoma", FontWeight.NORMAL, 200));
+        text.setFill(Color.rgb(255, 255, 255, 1.0));
+        text.setTextAlignment(TextAlignment.CENTER);
+        grid.add(text, 1, 1, 3, 1);
+
+//        Text text = new Text("???");
 //        text.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 //        text.setFill(Color.rgb(255, 255, 255, 1.0));
 //        grid.add(text, 1, 2, 1, 1);
+//
+//        Task<Void> task = new Task<Void>() {
+//            private long ONE_DAY = 5_000L; //86400000L;
+//
+//            @Override
+//            protected Void call() throws Exception {
+//                try {
+//                    while (true) {
+//                        System.out.println("Chore chart");
+//                        String person = "???";
+//                        int time = (Math.round((float) System.currentTimeMillis() / ONE_DAY) % 3);
+//                        if (time == 0)
+//                            person = "Noah";
+//                        else if (time == 1)
+//                            person = "Jesse";
+//                        else if (time == 2)
+//                            person = "Avi";
+//                        final String realPerson = person;
+//
+//                        Platform.runLater(() -> {
+//                            text.setText(realPerson);
+//                        });
+//
+//                        Thread.sleep(5_000);
+//                    }
+//                } catch (Exception exception) {
+//                    Log.line("Failed to change chore chart: " + exception);
+//                }
+//
+//                return null;
+//            }
+//        };
+//        Thread thread = new Thread(task);
+//        thread.setDaemon(true);
+//        thread.start();
 
         stage.setMaximized(true);
         stage.setFullScreen(true);
