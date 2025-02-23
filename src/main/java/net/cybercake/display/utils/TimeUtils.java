8b295e5b0981082c5f6afe7998b1a56ab6fe20f6
CyberedCake <157924032+nfranks8036@users.noreply.github.com>
2025-02-23 18:03:18 -0500

    Update TimeUtils.java

--- src/main/java/net/cybercake/display/utils/TimeUtils.java
+++ src/main/java/net/cybercake/display/utils/TimeUtils.java
@@ -6,9 +6,9 @@ public class TimeUtils {
         long seconds = duration % 60;
         long minutes = (duration / 60) % 60;
         long hours = (duration / 60 * 60) % 24;
-        return (hours > 0 ? hours + " hour" + (hours == 1 ? "" : "s") : "")
-                + (minutes > 0 ? minutes + " minute" + (minutes == 1 ? "" : "s") : "")
-                + (seconds + " second" + (seconds == 1 ? "" : "s"));
+        return (hours > 0 ? hours + "h" : "")
+                + (minutes > 0 ?  (hours > 0 ? " " : "") + minutes + "m" : "")
+                + (seconds > 0 ? (minutes > 0 ? " " : "") + seconds + "s" : "");
     }
 
 }
