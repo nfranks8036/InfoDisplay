40b423c18d2d10e59aef90080fcad70453a7a812
CyberedCake <157924032+nfranks8036@users.noreply.github.com>
2025-04-12 15:41:15 -0400

    Update WebPageManager.java

--- src/main/java/net/cybercake/display/browser/WebPageManager.java
+++ src/main/java/net/cybercake/display/browser/WebPageManager.java
@@ -30,7 +30,7 @@ public class WebPageManager {
         this.webPages = new ArrayList<>();
 
         String[] cefArgs = new String[]{
-                "--disable-gpu"
+                "--in-process-gpu"
         };
         if (!CefApp.startup(cefArgs)) {
             throw new RuntimeException("Failed to startup JCEF");
